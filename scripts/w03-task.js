4c3425bf2e1b108280382883306db88a3e3317ae
realtimestert <istewpotter1@gmail.com>
2023-09-30 22:28:41 -0700

    w03 task progress

---
+++ scripts/w03-task.js
@@ -0,0 +1,62 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2){
+    return number1 + number2;
+}
+
+function addNumbers(){
+    //const add1 = document.getElementById('add1');
+    let addNumber1 = Number(document.querySelector("#add1").value);
+    let addNumber2 = Number(document.querySelector("#add2").value);
+    document.querySelector("#sum").value = add(addNumber1, addNumber2);
+}
+
+document.querySelector("#addNumbers").addEventListener("click", addNumbers);
+
+/* Function Expression - Subtract Numbers */
+function subtract (number1, number2){
+    return number1 - number2;
+}
+
+function subtractNumbers(){
+    let subtract1 = Number(document.querySelector("#subtract1").value);
+    let subtract2 = Number(document.querySelector("#subtract2").value);
+    document.querySelector("#difference").value = subtract(subtract1, subtract2);
+}
+
+document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers);
+
+/* Arrow Function - Multiply Numbers */
+const multiply = (number1, number2) => {
+    return number1 * number2;
+}
+
+//factor1, factor2, product, multiplyNumbers
+const multiplyNumbers = () => {
+    let factor1 = number(document.querySelector("#factor1").value);
+    let factor2 = number(document.querySelector("#factor2").value);
+    document.querySelector("#product").value = multiply(factor1, factor2);
+}
+
+document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
