bec64dfcf0688608210126f58d69d566af3fe6a3
realtimestert <istewpotter1@gmail.com>
2023-10-18 22:04:18 -0700

    debugging

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -2,19 +2,30 @@
 
 /* Declare and initialize global variables */
 const templesElement = document.querySelector("#temples");
-const templeList = [];
+let templeList = [];
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    let article = document.createElement("article");
+    temples.foreach(temple => {
+        let article = document.createElement("article");
+        let templeName = document.createElement("h3");
+        templeName.textContent = temple.templeName;
+        let img = document.createElement("img");
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", temple.location);
+        article.appendChild(templeName);
+        article.appendChild(img);
+        templesElement.appendChild(article);
+    });
+    
 
-}
+};
 //for each temple in temples
 templesElement.appendChild(article);
 
 
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
-    let url = "https://byui-cse.github.io/cse121b-ww-course/resources/tesmples.json";
+    let url = "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json";
     const response = await fetch(url);
     if (response.ok){
         const templeList = await response.json();
@@ -28,9 +39,27 @@ let reset = ()=> {
 }
 
 /* sortBy Function */
-
-
+const sortBy = (temples) => {
+    reset();
+    let filter_ = document.querySelector("#sortBy").value;
+    switch (filter_) {
+        case "utah":
+            console.log(filter_);
+            break;
+        case "notutah":
+            console.log(filter_);
+            break;
+        case "older":
+            console.log(filter_);
+            break;
+        case "all":
+            console.log(filter_);
+            break;
+    }
+}
 
 getTemples();
 
 /* Event Listener */
+
+document.querySelector("#sortBy").addEventListener("change",() => sortBy(templeList));
